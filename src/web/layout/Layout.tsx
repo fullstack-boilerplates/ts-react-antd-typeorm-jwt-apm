bc7b1bfceb2a639c76d6a10500c5a55b52fa3570
junjun <postor@gmail.com>
2021-11-03 09:13:24 +0800

    Initial commit

---
+++ src/web/layout/Layout.tsx
@@ -0,0 +1,33 @@
+import { Layout as AntLayout, Spin } from 'antd'
+import { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Header } from './Header'
+import {AdminRoute} from './AdminRoute'
+
+const { Content, Footer } = AntLayout
+
+// lazy 作用： webpack 代码切分，避免单个 bundle 代码过大
+const Home = lazy(() => import('../pages/site/Home'))
+const Login = lazy(() => import('../pages/site/Login'))
+const Register = lazy(() => import('../pages/site/Register'))
+
+export const Layout = () => {
+  return <Router>
+    <AntLayout className="layout">
+      <Header />
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          <Suspense fallback={<Spin size="large" />}>
+            <Switch>
+              <Route path="/" component={Home} exact/>
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <AdminRoute />
+            </Switch>
+          </Suspense>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </AntLayout>
+  </Router>
+}
