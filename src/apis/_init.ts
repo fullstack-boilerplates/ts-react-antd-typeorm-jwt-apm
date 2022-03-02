fdf7ffdd52c6cabd314d5711df48ae0ab735eb28
postor <postor@gmail.com>
2022-03-02 17:09:00 +0800

    custom span

--- src/apis/_init.ts
+++ src/apis/_init.ts
@@ -2,7 +2,7 @@ import "reflect-metadata"
 import { join } from 'path'
 import { Express } from 'express'
 import Agent from 'elastic-apm-node'
-import { connect } from "../common/conn"
+import { connect as connectDB } from "../common/conn"
 
 interface Config {
   dev: boolean
@@ -13,13 +13,34 @@ interface Config {
   extension: string
 }
 
+
+export const beforeParser = async (app: Express, config: Config) => {
+  await connectDB()
+  useApm(app)
+}
+
 export default async (app: Express, config: Config) => {
+  app.use('*', (req, res) => res.sendFile(join(config.assetsFolder, 'index.html')))
+}
+
+function useApm(app: Express) {
   const { APM_SERVER = '' } = process.env
   if (APM_SERVER) {
-    Agent.start({
+    let apm = Agent.start({
       serverUrl: APM_SERVER,
     })
+    apm.addSpanFilter(payload => {
+      return payload.duration < 10 ? null : payload
+    })
+    app.use((req, res, next) => {
+      let { url } = req
+      let span = apm.startSpan(url, { childOf: 'url' })
+      let done = () => {
+        span.end()
+        res.off('close', done)
+      }
+      res.on('close', done)
+      next()
+    })
   }
-  await connect()
-  app.use('*', (req, res) => res.sendFile(join(config.assetsFolder, 'index.html')))
 }
